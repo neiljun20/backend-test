cff1976e0892d3cf2748864023c47011c9d1a078
Neiljun Cataag <neil@deskma.com>
2023-03-24 19:00:49 +0800

    adjust code

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,17 +2,21 @@ const userService = require('../services/user');
 
 exports.create = async (req, res) => {
 
+  let code = 500;
+  let msg = "account already exists";
+
   const isExist = await userService.isUserNameExist(req.body.userName);
   
   if(isExist){
-    res.status(500).send({ code: 500, msg: "account already exists" });
+    res.status(code).send({ code, msg });
     return;
   }
 
   await userService.createUser(req.body);
 
-  const code = 200;
+  code = 200;
+  msg = "OK"
 
-  res.status(code).send({ code, msg: "OK" });
+  res.status(code).send({ code, msg });
 
 };
